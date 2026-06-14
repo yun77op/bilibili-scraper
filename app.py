441ccb01915cd23fb2b3bdad406e21f72a9e9f3d
shuilong <yun77op@gmail.com>
2026-06-14 17:02:33 +0800

    PDF 字体改用 Android NotoSansSC

--- app.py
+++ app.py
@@ -1244,6 +1244,7 @@ def register_pdf_font() -> str:
     from reportlab.pdfbase.ttfonts import TTFont
 
     font_candidates = [
+        Path("C:/Windows/Fonts/NotoSansSC-VF.ttf"),
         Path("C:/Windows/Fonts/simsun.ttc"),
         Path("C:/Windows/Fonts/simhei.ttf"),
         Path("C:/Windows/Fonts/Deng.ttf"),
